19119be96ec81ae8686bc0c7f12e6f85769631b8
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-15 22:39:30 +0900

    Add file_write.c

---
+++ Ch2/file_write.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    int fd;
+    const char *buf = "My ship is solid!\n";
+    ssize_t nr;
+    size_t count;
+
+    fd = creat("./tmp.txt", 0644);
+    if (fd == -1) {
+        printf("failed to create a file");
+        exit(-1);
+    }
+    count = strlen(buf);
+    nr = write(fd, buf, strlen(buf));
+    if (nr == -1) {
+        printf("failed to write data\n");
+        exit(-1);
+    } else if (nr != count) {
+        printf("partial write happened\n");
+    }
+    return 0;
+}
