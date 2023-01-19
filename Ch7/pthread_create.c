578a40aaa68301232d73476c0388de1a061316c4
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-19 22:37:59 +0900

    Add pthread_create.c

---
+++ Ch7/pthread_create.c
@@ -0,0 +1,24 @@
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+
+void *start_routine(void* pArg) {
+    int *pVal = (int*) pArg;
+    printf("worker thread [%d]\n", *pVal);
+    *pVal = 200;
+}
+
+int main(void) {
+    pthread_t thread;
+    int ret, resp;
+
+    ret = pthread_create(&thread, NULL, start_routine, &resp);
+    if (ret) {
+        errno = ret;
+        perror("pthread_create");
+        return 1;
+    }
+    pthread_join(thread, NULL);
+
+    printf("response: %d\n", resp);
+}
