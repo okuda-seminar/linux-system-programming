4458a9741028cccdec70ad09037dcd3b6881a140
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-20 21:55:00 +0900

    Add clock_getres.c

---
+++ Ch11/clock_getres.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <errno.h>
+
+int main(void) {
+    clockid_t clocks[] = {
+        CLOCK_REALTIME,
+        CLOCK_MONOTONIC,
+        CLOCK_PROCESS_CPUTIME_ID,
+        CLOCK_THREAD_CPUTIME_ID,
+        CLOCK_MONOTONIC_RAW,
+        (clockid_t) -1
+    };
+
+    int i;
+
+    for (i = 0; clocks[i] != (clockid_t) -1; i++) {
+        struct timespec res;
+        int ret;
+
+        ret = clock_getres(clocks[i], &res);
+        if (ret) {
+            perror("clock_getres");
+        } else {
+            printf("clock=%d sec=%ld nsec=%ld\n", clocks[i], res.tv_sec, res.tv_nsec);
+        }
+    }
+
+    return 0;
+}
